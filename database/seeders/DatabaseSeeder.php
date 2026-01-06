932199fca4a8d96259206d3d3878b424c142dbcc
aungmyomyat67 <aungmyomyat046@gmail.com>
2026-01-06 09:55:14 +0630

    DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,30 +37,3 @@ class DatabaseSeeder extends Seeder
 }
 
 
-
-
-use App\Models\Profile;
-use App\Models\Post;
-use App\Models\Comment;
-
-class DatabaseSeeder extends Seeder
-{
-    public function run(): void
-    {
-        User::factory(10)->create()->each(function ($user) {
-
-            $user->profile()->create(Profile::factory()->make()->toArray());
-
-            $user->posts()->createMany(Post::factory(2)->make()->toArray());
-
-            foreach ($user->posts as $post) {
-                $post->comments()->createMany(
-                    Comment::factory(2)->make(['user_id' => $user->id])->toArray()
-                );
-            }
-
-            $randomPosts = Post::inRandomOrder()->take(2)->pluck('id');
-            $user->likedPosts()->attach($randomPosts);
-        });
-    }
-}
